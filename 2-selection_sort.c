c8ab3aa29ec317f12a2a4fee08fd3bac86ca048c
Luna Leguisamo <9356@holbertonstudents.com>
2024-08-11 22:11:04 +0000

    2-selection_sort.c

---
+++ 2-selection_sort.c
@@ -0,0 +1,34 @@
+#include "sort.h"
+
+/**
+ * selection_sort - function that sorts an array of integers
+ * in ascending order using the Selection sort algorithm
+ * @array: Array to sort
+ * @size: Size of the array
+ */
+
+void selection_sort(int *array, size_t size)
+{
+	size_t i, j;
+	int temp;
+	size_t minElement;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		minElement = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[minElement])
+			{
+				minElement = j;
+			}
+		}
+
+		if (minElement != i)
+		{
+			temp = array[i];
+			array[i] = array[minElement];
+			array[minElement] = temp;
+		}
+	}
+}
